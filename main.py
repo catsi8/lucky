4560dc4fd310cdc3daf2b32bf5bdaf2f4c8e2e6a
arguskao <arguskao@gmail.com>
2023-08-18 09:40:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
 import flet as ft
 import random
+import os 
+
+
+img_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),"img"
+)
 
 
 def main(page: ft.Page):
-    img = ft.Image(
-        src=f"/code1.jpg",
-        
-    )
-    images=ft.Image(src=f"./code1.jpg")
-    img2=ft.Image("/code1.jpg")
-    img3=ft.Image("./code1.jpg")
-    page.add(img)
+    images = ft.Row(expand=1, wrap=False, scroll="always")
     def click(e):
-        t.value = f"資料分別是: '{tb2.value}', '{tb3.value}', '{tb4.value}'"
+        
+        page.add(images)
+        i=random.randint(1, 61)
+        images.controls.append(
+            ft.Image(
+                #src=f"/Users/user/Dropbox/Code/python/Flet/work/static/img/A_{i}.png",
+                src=f"/img/A_{i}.png",
+                width=400,
+                height=900,
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(1),
+            )
+        )
         
         page.update()
         
-    def show(e):
-        t.value =random.randint(1, 10)
-        
-        page.update()    
-    #ft.TextField表示有一個輸入框，ft.Text表示直接印在螢幕上,ft.ElevatedButton表示有一個按鈕
-    t = ft.Text()
-    #tb1 = ft.TextField(label="Standard")
-    tb2 = ft.TextField(label="姓", hint_text="請輸入姓，例如：高",width=100)
-    tb3 = ft.TextField(label="名", hint_text="請輸入名字，例如：大俠",width=200)
-    tb4 = ft.TextField(label="手機", hint_text="請輸入手機號碼，例如；0980371712",width=300)
-    b = ft.ElevatedButton(text="送出", on_click=click)     #就是按下去執行後面的函數
-    page.add(ft.Row([tb2, tb3, tb4])) #排在同一行
-    page.add(b, t)
-    page.add()
-    #d=ft.Text("你是好人")
-    t2 = ft.ElevatedButton(text="按下去抽獎", on_click=show)     #就是按下去執行後面的函數
-    page.add(t2)
-    page.add(images,img2,img3)
+    def clean(e):
+        for img in images.controls:
+            images.controls.remove(img)
+        page.update()  
+
+    page.window_left=20
+    page.window_top=20
+    page.window_height = 1050
+    page.window_width =500
+    page.title = "觀音靈籤"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.padding = 50
     
     
-ft.app(target=main)
+    b = ft.ElevatedButton(text="卜卦", on_click=click)     #就是按下去執行後面的函數
+    c = ft.ElevatedButton(text="收回", on_click=clean)     #就是按下去執行後面的函數
+    #page.add(ft.Row([b,c])) #排在同一行
+    page.add(b,c,images)
+    
+    page.update()
+
+ft.app(target=main, view=ft.WEB_BROWSER, assets_dir='assets')
